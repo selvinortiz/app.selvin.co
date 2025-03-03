99b8c669718cc75d366216c4755646b79822ee0e
Selvin Ortiz <selvin@selvin.co>
2025-03-02 22:54:21 -0600

    Remove unused imports

--- app/Filament/Resources/HourResource.php
+++ app/Filament/Resources/HourResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\HourResource\Pages;
-use App\Filament\Resources\HourResource\RelationManagers;
 use App\Models\Hour;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class HourResource extends Resource
 {
